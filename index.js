ece1eba588874e35121ce3aae3e6ce3ff6a4e6a4
Abhinn Prakhar <abhinn.prakhar@gmail.com>
2025-07-20 00:25:43 +0530

    adding backend

---
+++ index.js
@@ -0,0 +1,38 @@
+import express, { request, response } from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors"
+
+const app = express();
+app.use(express.json());
+
+app.use(cors());
+
+//app.use(
+//    cors({
+//        origin: "http://localhost:3000",
+//       methods: ["GET", "POST", "PUT", "DELETE"],
+//        allowedheaders: ["Content-Type"],
+//    })
+//);
+
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(234).send('welcome')
+});
+
+app.use("/books", booksRoute);
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('app connected to db');
+        app.listen(PORT, () =>{
+        console.log(`app is listening to port : ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });

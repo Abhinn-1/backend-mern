760f5376f92e0f3a1eb14e1668e73fe41407c272
Abhinn-1 <137832176+Abhinn-1@users.noreply.github.com>
2025-07-20 04:35:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ app.use(express.json());
 
 app.use(cors());
 
-//app.use(
-//    cors({
-//        origin: "http://localhost:3000",
-//       methods: ["GET", "POST", "PUT", "DELETE"],
-//        allowedheaders: ["Content-Type"],
-//    })
-//);
+app.use(
+    cors({
+        origin: "https://frontend-mern-nu.vercel.app",
+       methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedheaders: ["Content-Type"],
+    })
+);
 
 app.get('/', (request, response) => {
     console.log(request)

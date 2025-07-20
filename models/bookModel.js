08c5dd28bf5b089bcc9223cb7b64601c8691a2a0
Abhinn-1 <137832176+Abhinn-1@users.noreply.github.com>
2025-07-20 19:21:45 +0530

    Update bookModel.js

--- models/bookModel.js
+++ models/bookModel.js
@@ -1,23 +1,32 @@
 import mongoose from "mongoose";
 
 const bookSchema = mongoose.Schema(
-    {
-        title: {
-            type: String,
-            required : true,
-        },
-        author: {
-            type: String,
-            required : true,
-        },
-        publishYear: {
-            type: Number,
-            required: true,
-        },
+  {
+    title: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true,
-    }
-); 
+    author: {
+      type: String,
+      required: true,
+    },
+    publishYear: {
+      type: Number,
+      required: true,
+    },
+    available: {
+      type: Boolean,
+      default: true,
+    },
+    returnDate: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
 
 export const Book = mongoose.model('Cat', bookSchema);

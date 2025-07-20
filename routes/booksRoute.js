6d22b3699c25e640016dd9b24a9a92d4287ef272
Abhinn-1 <137832176+Abhinn-1@users.noreply.github.com>
2025-07-20 19:32:31 +0530

    Update booksRoute.js

--- routes/booksRoute.js
+++ routes/booksRoute.js
@@ -4,33 +4,31 @@ import { Book } from "../models/bookModel.js";
 const router = express.Router();
 
 router.post('/', async (request, response) => {
-    try{
-        if (
-            !request.body.title ||
-            !request.body.author ||
-            !request.body.publishYear
-        ) 
-        {
-            return response.status(400).send({
-                message: 'send all required fields : title, author, publishYear', 
-            });
-        }
-        const newBook = {
-          title: request.body.title,
-          author: request.body.author,
-          publishYear: request.body.publishYear,
-          available: request.body.available ?? true,
-          returnDate: request.body.returnDate ?? null,
-        };
+  try {
+    const { title, author, publishYear, isAvailable, returnDate } = request.body;
 
-        const book = await Book.create(newBook);
-        return response.status(201).send(book);
+    if (!title || !author || !publishYear) {
+      return response.status(400).send({
+        message: 'Send all required fields: title, author, publishYear',
+      });
     }
-    catch(error){
-        console.log(error.message);
-        response.status(500).send({message: error.message});
-    }
+
+    const newBook = {
+      title,
+      author,
+      publishYear,
+      isAvailable: isAvailable ?? true,
+      returnDate: returnDate ?? null,
+    };
+
+    const book = await Book.create(newBook);
+    return response.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
 });
+
 
 router.get('/', async (request, response) => {
     try {
